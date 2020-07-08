0ab841ffa7598dd9f1103eb6c1f5135316494da0
Gitapim <57094137+Gitapim@users.noreply.github.com>
2020-07-08 22:40:27 +0200

    Create App.js

---
+++ Part1/UniCafe/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from "./components/Navbar"
+import './App.css';
+
+class App extends Component{
+
+  render(){
+  return (
+    <div className="container">
+      <Navbar/>
+    </div>
+  ); 
+  }
+}
+
+export default App;
